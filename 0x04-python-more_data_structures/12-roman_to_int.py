15dcee69dfd428840f0ba6cdaffcaf1104a33fda
rodgerpeter <rodgerpeter20@gmail.com>
2024-03-20 12:30:40 -0700

    Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -13,21 +13,3 @@ def roman_to_int(roman_string: str):
             rep -= numbers[i]
 
     return rep
-
-
-Advance task
-vi 100-weight_average.py
-
-#!/usr/bin/python3
-def weight_average(my_list=[]):
-    if not my_list:
-        return 0
-
-    num = 0
-    den = 0
-
-    for tup in my_list:
-        num += tup[0] * tup[1]
-        den += tup[1]
-
-    return (num / den)
